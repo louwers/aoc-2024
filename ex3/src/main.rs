3c4f3d21b0f133a2c5a1c220e66184f61ea6d44d
Bart Louwers <bart@emeel.net>
2024-12-06 00:14:13 +0100

    use do_mul

--- ex3/src/main.rs
+++ ex3/src/main.rs
@@ -9,22 +9,22 @@ fn main() -> io::Result<()> {
 
     let re = Regex::new(r"(mul\((\d+),(\d+)\))|(don't\(\))|(do\(\))").unwrap();
 
-    let mut doMul = true;
+    let mut do_mul = true;
     
     let mul: Vec<(i32, i32)> = re.captures_iter(&buffer).map(|caps| {
         let capt = caps.get(0).unwrap().as_str();
 
         if capt.starts_with("don't") {
-            doMul = false;
+            do_mul = false;
             return (0, 0);
         }
 
         if capt.starts_with("do") {
-            doMul = true;
+            do_mul = true;
             return (0, 0);
         }
 
-        if doMul && capt.starts_with("mul") {
+        if do_mul && capt.starts_with("mul") {
             let a = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
             let b = caps.get(3).unwrap().as_str().parse::<i32>().unwrap();
             return (a, b);

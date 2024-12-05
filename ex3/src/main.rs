3343e1dae13da382362ef7d1b7b60b7294cb0d72
Bart Louwers <bart@emeel.net>
2024-12-06 00:13:51 +0100

    day 3 part 2

--- ex3/src/main.rs
+++ ex3/src/main.rs
@@ -7,10 +7,29 @@ fn main() -> io::Result<()> {
 
     io::stdin().read_to_string(&mut buffer)?;
 
-    let re = Regex::new(r"mul\((\d+,\d+)\)").unwrap();
+    let re = Regex::new(r"(mul\((\d+),(\d+)\))|(don't\(\))|(do\(\))").unwrap();
+
+    let mut doMul = true;
+    
     let mul: Vec<(i32, i32)> = re.captures_iter(&buffer).map(|caps| {
-        let mut iter = caps.get(1).unwrap().as_str().split(",");
-        (iter.next().unwrap().parse::<i32>().unwrap(), iter.next().unwrap().parse::<i32>().unwrap())
+        let capt = caps.get(0).unwrap().as_str();
+
+        if capt.starts_with("don't") {
+            doMul = false;
+            return (0, 0);
+        }
+
+        if capt.starts_with("do") {
+            doMul = true;
+            return (0, 0);
+        }
+
+        if doMul && capt.starts_with("mul") {
+            let a = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+            let b = caps.get(3).unwrap().as_str().parse::<i32>().unwrap();
+            return (a, b);
+        }
+        (0, 0)
     }).collect();
     let total: i32 = mul.iter().map(|(a, b)| { a * b }).sum();
     println!("{total}");
